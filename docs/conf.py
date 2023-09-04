5d46ad9ef4f47685d1527132d5b9fa3302ad0cfd
Kacper Nowak <kacper.nowak@awi.de>
2023-09-04 14:18:12 +0200

    Mock dependencies for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+autodoc_mock_imports = ['numpy', 'scipy', 'cupy', 'cupyx', 'jax', 'xarray']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
