883b1549c4edb27213f5d65732cb7d578674684c
Kacper Nowak <kacper.nowak@awi.de>
2024-06-03 11:15:15 +0200

    Add NemoNumpyFilter to __init__.py

--- implicit_filter/__init__.py
+++ implicit_filter/__init__.py
@@ -1,5 +1,6 @@
 from .jax_filter import JaxFilter
 from .numpy_filter import NumpyFilter
+from .nemo_filter import NemoNumpyFilter
 from ._auxiliary import make_tri, convert_to_wavenumbers
 
 # If CuPy is not installed this class won't be imported
