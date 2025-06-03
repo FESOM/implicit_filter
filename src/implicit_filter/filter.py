7f367a54f8951614b4d091dd0794315a1c45e356
Kacper Nowak <kacper.nowak@awi.de>
2025-06-03 18:15:19 +0200

    Refine abstract Filter class to work with all types of filters

--- src/implicit_filter/filter.py
+++ src/implicit_filter/filter.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, List, Iterable
+from typing import Tuple, Iterable
 
 import numpy as np
 
@@ -39,12 +39,9 @@ class Filter(ABC):
         pass
 
     @abstractmethod
-    def compute_velocity(self, n: int, k: float, ux: np.ndarray, uy: np.ndarray, interp: bool = True) -> Tuple[np.ndarray, np.ndarray]:
+    def compute_velocity(self, n: int, k: float, ux: np.ndarray, uy: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         Compute the filtered velocity data using a specified filter size.
-
-        It performs interpolates it from elements to nodes if requested.
-        Output is always based on mesh nodes.
 
         Parameters:
         -----------
@@ -70,88 +67,12 @@ class Filter(ABC):
         """
         pass
 
-    @abstractmethod
-    def compute_on_cells(self, n: int, k: float, ux: np.ndarray) -> np.ndarray:
-        """
-        Compute the filtered velocity data using a specified filter size.
-
-        It performs implicit interpolates it from elements to nodes. Currently, filtering directly on elements
-        is not supported.
-
-        Parameters:
-        -----------
-        n : int
-            Order of filter, one is recommended
-
-        k : float
-            Wavelength of the filter.
-
-        ux : np.ndarray
-            NumPy array containing eastward velocity component to be filtered.
-
-        Returns:
-        --------
-        Tuple[np.ndarray, np.ndarray]:
-            Tuple containing NumPy arrays with filtered data ux and uy velocities.
-        """
-        pass
 
     @abstractmethod
-    def many_compute(self, n: int, k: float, data: np.ndarray | List[np.ndarray]) -> List[np.ndarray]:
+    def compute_spectra_scalar(self, n: int, k: Iterable | np.ndarray, data : np.ndarray, highpass : bool,
+                               mask: np.ndarray | None = None) -> np.ndarray:
         """
-        Computes multiple inputs, which are scalar data
-
-        Parameters:
-        -----------
-        n : int
-            Order of filter, one is recommended
-
-        k : float
-            Wavelength of the filter.
-
-        data : np.ndarray | List[np.ndarray]
-            It can be either a list of 1D NumPy arrays to be processed or
-            a 2D NumPy array which second dimension will be iterated over.
-
-        Returns:
-        --------
-        List[np.ndarray]:
-            List containing NumPy arrays with filtered data
-        """
-        pass
-
-    def many_compute_velocity(self, n: int, k: float, ux: np.ndarray | List[np.ndarray],
-                              vy: np.ndarray | List[np.ndarray]) -> Tuple[List[np.ndarray], List[np.ndarray]]:
-        """
-        Computes multiple velocity inputs
-
-        Parameters:
-        -----------
-        n : int
-            Order of filter, one is recommended
-
-        k : float
-            Wavelength of the filter.
-
-        ux : np.ndarray
-            Eastward velocity component to be filtered. It can be either a list of 1D NumPy arrays to be processed or
-            a 2D NumPy array which 2nd dimension will be iterated over.
-
-        uy : np.ndarray
-            Northwards velocity component to be filtered. It can be either a list of 1D NumPy arrays to be processed or
-            a 2D NumPy array which 2nd dimension will be iterated over.
-
-        Returns:
-        --------
-        Tuple[List[np.ndarray], List[np.ndarray]]:
-            Tuple containing lists containing NumPy arrays with filtered data
-        """
-        pass
-
-    @abstractmethod
-    def compute_spectra_scalar(self, n: int, k: Iterable | np.ndarray, data : np.ndarray, highpass : bool) -> np.ndarray:
-        """
-        Computes spectra for given wavelengths.
+        Computes power spectra for given wavelengths.
 
         The spectra can be computed using highpass or lowpass filtering technique.
         For details refer to https://arxiv.org/abs/2404.07398
@@ -169,6 +90,11 @@ class Filter(ABC):
         highpass : bool
             If true, highpass filtering is used.
 
+        mask : np.ndarray | None
+            Mask applied to data while computing spectra.
+            True means selected data won't be used for computing spectra.
+            This mask won't be used during filtering.
+
         Returns:
         --------
         np.ndarray:
@@ -177,9 +103,10 @@ class Filter(ABC):
         pass
 
     @abstractmethod
-    def compute_spectra_velocity(self, n: int, k: Iterable | np.ndarray, ux: np.ndarray | List[np.ndarray], highpass: bool, interp: bool = True) -> np.ndarray:
+    def compute_spectra_velocity(self, n: int, k: Iterable | np.ndarray, ux: np.ndarray, vy: np.ndarray,
+                                 highpass: bool, mask: np.ndarray | None = None, interp: bool = True) -> np.ndarray:
         """
-        Computes spectra for given wavelengths.
+        Computes power spectra for given wavelengths.
 
         The spectra can be computed using highpass or lowpass filtering technique.
         For details refer to https://arxiv.org/abs/2404.07398
@@ -192,13 +119,18 @@ class Filter(ABC):
             List of wavelengths to be filtered.
 
         ux : np.ndarray
-            NumPy array containing eastward velocity component to be filtered.
+            NumPy array containing an eastward velocity component to be filtered.
 
         uy : np.ndarray
-            NumPy array containing northwards velocity component to be filtered.
+            NumPy array containing a northwards velocity component to be filtered.
 
         highpass : bool
             If true, highpass filtering is used.
+
+        mask : np.ndarray | None
+            Mask applied to data while computing spectra.
+            True means selected data won't be used for computing spectra.
+            This mask won't be used during filtering.
 
         interp : bool
             If true, interpolate data from elements to nodes.
@@ -216,5 +148,5 @@ class Filter(ABC):
 
     @classmethod
     def load_from_file(cls, file: str):
-        """Load auxiliary arrays from file"""
+        """Load auxiliary arrays from a file"""
         return cls(**dict(np.load(file)))
