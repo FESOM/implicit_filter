Fix icon masking

src/implicit_filter/icon_filter.py
@@ -8,8 +8,8 @@ class IconFilter(TriangularFilter):
     def prepare_from_file(
         self,
         grid_file: str,
-        ocean_mask: np.ndarray | bool = False,
         full: bool = False,
+        mask: np.ndarray | bool = False,
         gpu: bool = False,
     ):
         grid2d = xr.open_dataset(grid_file)
@@ -18,18 +18,20 @@ class IconFilter(TriangularFilter):
         ycoord = grid2d["vlat"].values * 180.0 / np.pi  # Location of nodes, in degrees
         tri = grid2d["vertex_of_cell"].values.T.astype(int) - 1
         tri = tri.astype(int)
+        mask_transform = False
 
-        if isinstance(ocean_mask, np.ndarray):
+        if isinstance(mask, np.ndarray):
             pass
-        elif ocean_mask:
+        elif mask:
             if "cell_sea_land_mask" in grid2d:
-                ocean_mask = grid2d["cell_sea_land_mask"].values
+                mask = grid2d["cell_sea_land_mask"].values
+                mask_transform = True
             else:
                 raise KeyError(
                     f"In the file {grid_file} there's no ocean mask under default name {'cell_sea_land_mask'}"
                 )
         else:
-            ocean_mask = np.ones(len(tri[:, 1]))
+            mask = np.ones(len(tri[:, 1]))
             # NOTE: LSM is in grid2d['cell_sea_land_mask'] or in grid3d['lsm_c'].isel(depth=???)
 
         self.prepare(
@@ -42,6 +44,7 @@ class IconFilter(TriangularFilter):
             cartesian=False,
             cyclic_length=2.0 * np.pi,
             full=full,
-            mask=ocean_mask,
+            mask=mask,
             gpu=gpu,
+            mask_transform=mask_transform,
         )

src/implicit_filter/latlon_filter.py
@@ -67,6 +67,7 @@ class LatLonFilter(Filter):
         longitude: np.ndarray,
         cartesian: bool = False,
         local: bool = True,
+        mask: np.ndarray | None = None,
         gpu: bool = False,
     ):
         """
@@ -102,6 +103,8 @@ class LatLonFilter(Filter):
 
         xcoord = np.reshape(xcoord, [nx * ny])
         ycoord = np.reshape(ycoord, [nx * ny])
+
+        self._mask_n = np.ones(self._e2d, dtype=bool) if mask is None else mask
 
         if local:
             ee_pos, nza = calculate_local_regular_neighbourhood(e2d, nx, ny)
@@ -183,6 +186,13 @@ class LatLonFilter(Filter):
             jj[no] = n
             ss[no] = -np.sum(ss[no : nn + 1])
             nn += 1
+
+        # Create a mask where both _ii and _jj are not 0
+        mask_sp = self._mask_n[self._ii] & self._mask_n[self._jj]
+
+        self._ss = self._ss[mask_sp]
+        self._ii = self._ii[mask_sp]
+        self._jj = self._jj[mask_sp]
 
         self._ss = ss
         self._ii = ii
@@ -340,7 +350,7 @@ class LatLonFilter(Filter):
         ) / np.sum(selected_area)
 
         for i in range(nr):
-            ttu, ttv = self.compute_velocity(n, k[i], unod, vnod)
+            ttu, ttv = self.compute_velocity(n, k[i], unod, vyn)
 
             ttu -= unod
             ttv -= vnod

src/implicit_filter/nemo_filter.py
@@ -11,7 +11,13 @@ class NemoFilter(LatLonFilter):
     A filter class for NEMO ocean model data using NumPy arrays.
     """
 
-    def prepare_from_file(self, file: str, vl: int, gpu: bool = False):
+    def prepare_from_file(
+        self,
+        file: str,
+        vl: int,
+        mask: np.ndarray | bool = True,
+        gpu: bool = False,
+    ):
         ds = xr.open_dataset(file)
 
         nx, ny = (
@@ -125,12 +131,18 @@ class NemoFilter(LatLonFilter):
         ss = np.zeros(nza, dtype="float")
         ii = np.zeros(nza, dtype="int")
         jj = np.zeros(nza, dtype="int")
-        mask = np.reshape(
-            ds.tmask.isel(t=0, z=vl, y=slice(None, -2), x=slice(None, -2))
-            .transpose("x", "y")
-            .values,
-            nx * ny,
-        )
+
+        if isinstance(mask, np.ndarray):
+            pass
+        elif mask:
+            mask = np.reshape(
+                ds.tmask.isel(t=0, z=vl, y=slice(None, -2), x=slice(None, -2))
+                .transpose("x", "y")
+                .values,
+                nx * ny,
+            )
+        else:
+            mask = np.ones(nx * ny, dtype=bool)
 
         nn = 0
         for n in range(e2d):

src/implicit_filter/triangular_filter.py
@@ -18,7 +18,6 @@ from implicit_filter.utils._jax_function import (
     make_smat,
     make_smat_full,
     transform_mask_to_nodes,
-    transform_vector_to_nodes,
 )
 from implicit_filter.utils.utils import (
     SolverNotConvergedError,
@@ -205,12 +204,15 @@ class TriangularFilter(Filter):
         full: bool = False,
         mask: np.ndarray = None,
         gpu: bool = False,
+        mask_transform: bool = False,
     ):
         # NOTE: xcoord & ycoord are in degrees, but cyclic_length is in radians
+        self._n2d = n2d
+        self._e2d = e2d
+        self._full = full
 
         if mask is None:
             mask = np.ones(e2d)
-
         ne_num, ne_pos = neighboring_triangles(n2d, e2d, tri)
         nn_num, nn_pos = neighbouring_nodes(n2d, tri, ne_num, ne_pos)
         area, elem_area, dx, dy, Mt = areas(
@@ -224,7 +226,6 @@ class TriangularFilter(Filter):
             meshtype,
             cartesian,
             cyclic_length,
-            mask,
         )
 
         self._elem_area = jnp.array(elem_area)
@@ -241,9 +242,13 @@ class TriangularFilter(Filter):
 
         self._mask_n = jnp.array(mask)
 
-        # self._mask_n = jnp.where(self._mask_n > 0.5, 1.0, 0.0).astype(
-        #     bool
-        # )  # Where there's ocean
+        if mask_transform:
+            self._mask_n = transform_mask_to_nodes(
+                self._mask_n, self._ne_pos, self._ne_num, self._n2d
+            )
+            self._mask_n = ~jnp.where(self._mask_n > 0.5, 1.0, 0.0).astype(
+                bool
+            )  # Where there's ocean
 
         smooth, metric = make_smooth(
             jMt,
@@ -270,20 +275,17 @@ class TriangularFilter(Filter):
         ## Set rows (and columns!) of smooth where (node) mask is 0 (land) to 0: This enforces a Neumann BC
         #   i.e. Set _ss = 0 where mask_n[_ii] = 0 && mask_n[_jj] = 0
         # AFW
-
         # Create a mask where both _ii and _jj are not 0
         if full:
-            mask_sp = self._mask_n[self._ii % n2d] & self._mask_n[self._jj % n2d]
+            mask_sp = jnp.logical_and(
+                self._mask_n[self._ii % n2d], self._mask_n[self._jj % n2d]
+            )
         else:
             mask_sp = jnp.logical_and(self._mask_n[self._ii], self._mask_n[self._jj])
 
         self._ss = self._ss[mask_sp]
         self._ii = self._ii[mask_sp]
         self._jj = self._jj[mask_sp]
-
-        self._n2d = n2d
-        self._e2d = e2d
-        self._full = full
 
         self.set_backend("gpu" if gpu else "cpu")
 

src/implicit_filter/utils/_auxiliary.py
@@ -62,7 +62,9 @@ def neighboring_triangles(n2d: int, e2d: int, tri: np.ndarray):
     return ne_num, ne_pos
 
 
-def neighbouring_nodes(n2d: int, tri: np.ndarray, ne_num: np.ndarray, ne_pos: np.ndarray):
+def neighbouring_nodes(
+    n2d: int, tri: np.ndarray, ne_num: np.ndarray, ne_pos: np.ndarray
+):
     """
     Compute neighboring nodes for each node in a 2D mesh.
 
@@ -93,7 +95,6 @@ def neighbouring_nodes(n2d: int, tri: np.ndarray, ne_num: np.ndarray, ne_pos: np
         for each node. The 'nn_pos[i, j]' entry indicates the index of the i-th neighboring node for the j-th node.
     """
     # Initialize an array to store the count of neighboring nodes for each node.
-
 
     # Initialize an array to store the positions of neighboring nodes for each node.
     nn_num = np.zeros([n2d], dtype=int)
@@ -132,9 +133,18 @@ def neighbouring_nodes(n2d: int, tri: np.ndarray, ne_num: np.ndarray, ne_pos: np
     return nn_num, nn_pos
 
 
-
-def areas(n2d: int, e2d: int, tri: np.ndarray, xcoord: np.ndarray, ycoord: np.ndarray, ne_num: np.ndarray,
-          ne_pos: np.ndarray, meshtype: str, carthesian: bool, cyclic_length, mask: np.ndarray):
+def areas(
+    n2d: int,
+    e2d: int,
+    tri: np.ndarray,
+    xcoord: np.ndarray,
+    ycoord: np.ndarray,
+    ne_num: np.ndarray,
+    ne_pos: np.ndarray,
+    meshtype: str,
+    carthesian: bool,
+    cyclic_length,
+):
     """
     Calculate areas of triangles and derivatives of P1 basis functions.
 
@@ -196,7 +206,7 @@ def areas(n2d: int, e2d: int, tri: np.ndarray, xcoord: np.ndarray, ycoord: np.nd
     r_earth = 6400  # Earth's radius, assuming units in kilometers
     Mt = np.ones([e2d])
 
-    if meshtype == 'm':
+    if meshtype == "m":
         for n in range(e2d):
             # Calculate differences in x and y coordinates for triangle vertices.
             x2 = xcoord[tri[n, 1]] - xcoord[tri[n, 0]]
@@ -219,7 +229,7 @@ def areas(n2d: int, e2d: int, tri: np.ndarray, xcoord: np.ndarray, ycoord: np.nd
             # Calculate the area of the triangle.
             elem_area[n] = 0.5 * abs(d)
 
-    elif meshtype == 'r':
+    elif meshtype == "r":
         rad = math.pi / 180.0
         if carthesian:
             Mt = np.ones([e2d])
@@ -257,7 +267,7 @@ def areas(n2d: int, e2d: int, tri: np.ndarray, xcoord: np.ndarray, ycoord: np.nd
             dy[n, 2] = x2 / d
 
             # Calculate the area of the triangle.
-            elem_area[n] = 0.5 * abs(d) * mask[n]
+            elem_area[n] = 0.5 * abs(d)
 
         if carthesian:
             Mt = np.zeros([e2d])
@@ -267,7 +277,7 @@ def areas(n2d: int, e2d: int, tri: np.ndarray, xcoord: np.ndarray, ycoord: np.nd
     # Calculate scalar cell (cluster) area for each node.
     area = np.zeros([n2d])
     for n in range(n2d):
-        area[n] = np.sum(elem_area[ne_pos[0:ne_num[n], n]]) / 3.0
+        area[n] = np.sum(elem_area[ne_pos[0 : ne_num[n], n]]) / 3.0
         if area[n] == 0.0:
             area[n] = 1.0e-5
 
@@ -362,7 +372,9 @@ def convert_to_wavenumbers(dist, dxm):
     return 2 * math.pi / size
 
 
-def find_adjacent_points_north(mesh_mask_path: str = None, lon_lat_prec_degrees: float = None) -> tuple[Series, int]:
+def find_adjacent_points_north(
+    mesh_mask_path: str = None, lon_lat_prec_degrees: float = None
+) -> tuple[Series, int]:
     """
     Fix rounding erros in NEMO grid using linear regression
 
@@ -398,8 +410,8 @@ def find_adjacent_points_north(mesh_mask_path: str = None, lon_lat_prec_degrees:
 
     # Find out which row corresponds to the last one y
     if (
-            abs(np.sort(ilon.isel(y=-1)) - np.sort(ilon.isel(y=-2))).mean()
-            < abs(np.sort(ilon.isel(y=-1)) - np.sort(ilon.isel(y=-3))).mean()
+        abs(np.sort(ilon.isel(y=-1)) - np.sort(ilon.isel(y=-2))).mean()
+        < abs(np.sort(ilon.isel(y=-1)) - np.sort(ilon.isel(y=-3))).mean()
     ):
         corresponds_to_redundant = -2
     else:
@@ -408,14 +420,22 @@ def find_adjacent_points_north(mesh_mask_path: str = None, lon_lat_prec_degrees:
 
     # extract redundant (last row) and correspoding row coords
     ilon_redundant = ilon.isel(x=slice(1, -1)).isel(y=-1, drop=True)
-    ilon_corresponds = ilon.isel(x=slice(1, -1)).isel(y=corresponds_to_redundant, drop=True)
+    ilon_corresponds = ilon.isel(x=slice(1, -1)).isel(
+        y=corresponds_to_redundant, drop=True
+    )
     ilat_redundant = ilon.isel(x=slice(1, -1)).isel(y=-1, drop=True)
-    ilat_corresponds = ilon.isel(x=slice(1, -1)).isel(y=corresponds_to_redundant, drop=True)
+    ilat_corresponds = ilon.isel(x=slice(1, -1)).isel(
+        y=corresponds_to_redundant, drop=True
+    )
 
     # find corresponding x
     x_corr = []
-    for x_r, lon_r, lat_r in list(zip(ilon_redundant.x.data, ilon_redundant.data, ilat_redundant.data)):
-        for x_c, lon_c, lat_c in zip(ilon_corresponds.x.data, ilon_corresponds.data, ilat_corresponds.data):
+    for x_r, lon_r, lat_r in list(
+        zip(ilon_redundant.x.data, ilon_redundant.data, ilat_redundant.data)
+    ):
+        for x_c, lon_c, lat_c in zip(
+            ilon_corresponds.x.data, ilon_corresponds.data, ilat_corresponds.data
+        ):
             if lon_r.data[()] == lon_c.data[()]:
                 if lat_r.data[()] == lat_c.data[()]:
                     if x_c not in x_corr:
@@ -423,14 +443,30 @@ def find_adjacent_points_north(mesh_mask_path: str = None, lon_lat_prec_degrees:
                         break
 
     # filter adjacent x for outliers
-    adjacent_x = pd.Series(x_corr, name="adjacent_x", index=pd.Series(ilon_redundant.x.data, name="reference_x"))
-    adjacent_x_sanitized = adjacent_x.where(abs(adjacent_x.diff()) < 1.5 * abs(adjacent_x.diff()).mean()).dropna()
+    adjacent_x = pd.Series(
+        x_corr,
+        name="adjacent_x",
+        index=pd.Series(ilon_redundant.x.data, name="reference_x"),
+    )
+    adjacent_x_sanitized = adjacent_x.where(
+        abs(adjacent_x.diff()) < 1.5 * abs(adjacent_x.diff()).mean()
+    ).dropna()
 
     # fit clean adjacent indices
     lr = LinearRegression()
-    lr.fit(np.array(adjacent_x_sanitized.index).reshape(-1, 1), np.array(adjacent_x_sanitized).reshape(-1, 1))
-    adjacent_x_fit = pd.Series(lr.predict(np.array(adjacent_x.index).reshape(-1, 1)).reshape(-1),
-                               index=adjacent_x.index, name="adjacent_x").round().astype(int)
+    lr.fit(
+        np.array(adjacent_x_sanitized.index).reshape(-1, 1),
+        np.array(adjacent_x_sanitized).reshape(-1, 1),
+    )
+    adjacent_x_fit = (
+        pd.Series(
+            lr.predict(np.array(adjacent_x.index).reshape(-1, 1)).reshape(-1),
+            index=adjacent_x.index,
+            name="adjacent_x",
+        )
+        .round()
+        .astype(int)
+    )
 
     # test if we were successful
     np.testing.assert_array_almost_equal(
@@ -440,4 +476,3 @@ def find_adjacent_points_north(mesh_mask_path: str = None, lon_lat_prec_degrees:
 
     # return adjacent indices
     return adjacent_x_fit, corresponds_to_redundant
-