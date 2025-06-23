e6051a0c196eb47562f89a94c32366cb290522e3
Kacper Nowak <kacper.nowak@awi.de>
2025-06-23 17:26:02 +0200

    Change __init__

--- src/implicit_filter/__init__.py
+++ src/implicit_filter/__init__.py
@@ -5,4 +5,9 @@ from .icon_filter import IconFilter
 from .nemo_filter import NemoFilter
 from .latlon_filter import LatLonFilter
 from .utils._auxiliary import make_tri, convert_to_wavenumbers
-from .utils.conversion_tools import transform_velocity_to_nodes, transform_scalar_to_nodes
+from .utils.conversion_tools import (
+    transform_velocity_to_nodes,
+    transform_scalar_to_nodes,
+    transform_mask_from_elements_to_nodes,
+    transform_mask_from_nodes_to_elements,
+)
