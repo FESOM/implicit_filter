ccd79c3dd3ccf80cab79b3c4e74490901c1c95d5
Kacper Nowak <kacper.nowak@awi.de>
2025-12-05 12:48:02 +0100

    Update example and readme

--- src/implicit_filter/__init__.py
+++ src/implicit_filter/__init__.py
@@ -4,7 +4,7 @@ from .fesom_filter import FesomFilter
 from .icon_filter import IconFilter
 from .nemo_filter import NemoFilter
 from .latlon_filter import LatLonFilter
-from .utils._auxiliary import make_tri, convert_to_wavenumbers
+from .utils._auxiliary import make_tri
 from .utils.conversion_tools import (
     transform_velocity_to_nodes,
     transform_scalar_to_nodes,
